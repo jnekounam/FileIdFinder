c5607ad2abd235b0b41d21fce2e2034f703fd42b
mhjavad <jnekounam@yahoo.com>
2017-02-16 10:00:35 +0330

    second bot

---
+++ mec94_bot/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace mec94_bot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(67, 41);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "mec94";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ mec94_bot/Form1.cs
@@ -0,0 +1,61 @@
+﻿using NetTelegramBotApi;
+using NetTelegramBotApi.Requests;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mec94_bot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backgroundWorker1.InitializeLifetimeService();
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Task.Run(() => Runbot());
+        }
+        public static async Task Runbot()
+        {
+            var bot = new TelegramBot("339493278:AAH-4bouLgr2RGNhiqBujxSdyYUw2M5wPGU");
+            long offset = 0;
+            while (true)
+            {
+                var updates = await bot.MakeRequestAsync(new GetUpdates() { Offset = offset });
+                foreach (var update in updates)
+                {
+                    offset = update.UpdateId + 1;
+                    var text = update.Message.Text;
+                    var file = update.Message.Document;
+                    if (file != null)
+                    {
+                        var req = new SendMessage(update.Message.Chat.Id, "File ID: " + file.FileId.ToString());
+                        await bot.MakeRequestAsync(req);
+                        continue;
+                    }
+                    else if (text != null)
+                    {
+                        var req = new SendMessage(update.Message.Chat.Id, "ID: " + update.Message.From.Id);
+                        await bot.MakeRequestAsync(req);
+                        continue;
+                    }
+
+                }
+            }
+        }
+    }
+}
